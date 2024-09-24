1954f182a3b09c1edc5bc5f441dab5a6870a64af
NihalaBasith <103995690+NihalaBasith@users.noreply.github.com>
2024-09-24 16:54:01 +0400

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -10,16 +10,16 @@ if (!isset($_SESSION['user_id'])) {
 if (isset($_GET['id'])) {
     $file_id = $_GET['id'];
     
-    // Fetch the file details
+   
     $query = "SELECT * FROM files WHERE id='$file_id' AND user_id='" . $_SESSION['user_id'] . "'";
     $result = mysqli_query($conn, $query);
     $file = mysqli_fetch_assoc($result);
 
     if ($file) {
-        // Delete file from the server
+       
         unlink($file['file_path']);
         
-        // Remove file from the database
+        
         $query = "DELETE FROM files WHERE id='$file_id'";
         mysqli_query($conn, $query);
 
